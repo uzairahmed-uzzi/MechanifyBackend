32353bea5e7c1658b1a7b7ceb055362dfef29d93
Uzair Ahmed <uzziahmed74@gmail.com>
2024-02-24 10:52:45 +0500

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const {generateToken, verifyToken} = require("../middlewares/jwt");
 const _ = require("lodash");
 const cloudinary = require('cloudinary').v2;
-const multer = require('multer')
+
 
 
 // Configure Cloudinary
