b9c7575044bdce02bc59ab55d7faf60f5f18e644
Uzair Ahmed <uzziahmed74@gmail.com>
2024-02-25 12:24:22 +0500

    .

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        default: "user",
+        default: "Requestor",
         required: true
     },
     latitude:{
